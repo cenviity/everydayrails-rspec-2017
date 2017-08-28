595254366c7104bdbdd959d0df88f03ff2b6b482
Vincent Yim <cenviity@gmail.com>
2017-08-28 13:06:40 +0000

    Add spec for user invalid with a duplicate email address

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,23 @@ RSpec.describe User, type: :model do
   end
   
   it "is invalid without an email address"
-  it "is invalid with a duplicate email address"
+  
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name: "Joe",
+      last_name:  "Tester",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze"
+    )
+    user = User.new(
+      first_name: "Jane",
+      last_name:  "Tester",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze"
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+  
   it "returns a user's full name as a string"
 end
