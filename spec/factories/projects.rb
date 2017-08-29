66d8c2db6e2b3a9e4f4ca6f63e41e4edcc415284
Vincent Yim <cenviity@gmail.com>
2017-08-29 15:25:20 +0000

    Add spec for #create as an authenticated user with invalid attributes

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -20,6 +20,10 @@ FactoryGirl.define do
     trait :due_tomorrow do
       due_on 1.day.from_now
     end
+    
+    trait :invalid do
+      name nil
+    end
 
     # Factory inheritance examples ...
     #
