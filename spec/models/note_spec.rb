2b41d54c52598b45a032f40aff021fde754be96a
Vincent Yim <cenviity@gmail.com>
2017-08-29 12:02:18 +0000

    Add spec for note generating associated data from a factory

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe Note, type: :model do
       name: "Test Project",
     )
   end
+  
+  it "generates associated data from a factory" do
+    note = create(:note)
+    puts "This note's project is #{note.project.inspect}"
+    puts "This note's user is #{note.user.inspect}"
+  end
 
   it "is valid with a user, project, and message" do
     note = Note.new(
