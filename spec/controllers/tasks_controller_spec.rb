c65c3c3ffdd3275265c900512a0d4afc104f9e8f
Vincent Yim <cenviity@gmail.com>
2017-08-29 15:37:23 +0000

    Add specs for #create

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -15,4 +15,33 @@ RSpec.describe TasksController, type: :controller do
       expect(response.content_type).to eq "application/json"
     end
   end
+  
+  describe "#create" do
+    it "responds with JSON formatted output" do
+      new_task = { name: "New test task" }
+      sign_in @user
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task }
+      expect(response.content_type).to eq "application/json"
+    end
+    
+    it "adds a new task to the project" do
+      new_task = { name: "New test task" }
+      sign_in @user
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to change(@project.tasks, :count).by(1)
+    end
+    
+    it "requires authentication" do
+      new_task = { name: "New test task" }
+      # Don't sign in this time…
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.not_to change(@project.tasks, :count)
+      expect(response).not_to be_success
+    end
+  end
 end
