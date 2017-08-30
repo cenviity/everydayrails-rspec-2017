6e568d773eb4088e5a14f8099ac1b0aa498f3d4f
Vincent Yim <cenviity@gmail.com>
2017-08-30 16:25:57 +0000

    Add spec for home page responding successfully

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /homes" do
-    it "works! (now write some real specs)" do
-      get homes_path
-      expect(response).to have_http_status(200)
-    end
+RSpec.describe "Home page", type: :request do
+  it "responds successfully" do
+    get root_path
+    expect(response).to be_success
+    expect(response).to have_http_status "200"
   end
 end
