b9dbce2ca60898a008233a5d5c2d8a10be0f51cc
Vincent Yim <cenviity@gmail.com>
2017-08-30 12:16:55 +0000

    Add scenario for user toggling a task

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "user toggles a task", js: true do
+    user = create(:user)
+    project = create(:project,
+      name: "RSpec tutorial",
+      owner: user)
+    task = project.tasks.create!(name: "Finish RSpec tutorial")
+    
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    
+    click_link "RSpec tutorial"
+    check "Finish RSpec tutorial"
+    
+    expect(page).to have_css "label#task_#{task.id}.completed"
+    expect(task.reload).to be_completed
+    
+    uncheck "Finish RSpec tutorial"
+    
+    expect(page).not_to have_css "label#task_#{task.id}.completed"
+    expect(task.reload).not_to be_completed
+  end
 end
