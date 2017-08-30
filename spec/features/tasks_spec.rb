2942bc569eb5c7b4a7a20d44b29e5026635bd83b
Vincent Yim <cenviity@gmail.com>
2017-08-30 20:19:32 +0000

    Refactor with helper methods

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,24 +1,50 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  scenario "user toggles a task", js: true do
-    user = FactoryGirl.create(:user)
-    project = FactoryGirl.create(:project,
+  let(:user) { create(:user) }
+  let(:project) {
+    create(:project,
       name: "RSpec tutorial",
-      owner: user)
-    task = project.tasks.create!(name: "Finish RSpec tutorial")
-    
+      owner: user
+    )
+  }
+  let!(:task) { project.tasks.create!(name: "Finish RSpec tutorial") }
+  
+  scenario "user toggles a task", js: true do
     login_as user, scope: :user
+    go_to_project "RSpec tutorial"
+    
+    complete_task "Finish RSpec tutorial"
+    expect_complete_task "Finish RSpec tutorial"
+    
+    undo_complete_task "Finish RSpec tutorial"
+    expect_incomplete_task "Finish RSpec tutorial"
+  end
+  
+  def go_to_project(name)
     visit root_path
-    click_link "RSpec tutorial"
-    check "Finish RSpec tutorial"
-
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to be_completed
-
-    uncheck "Finish RSpec tutorial"
-
-    expect(page).to_not have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to_not be_completed
+    click_link name
+  end
+  
+  def complete_task(name)
+    check name
+  end
+  
+  def undo_complete_task(name)
+    uncheck name
+  end
+  
+  def expect_complete_task(name)
+    aggregate_failures do
+      expect(page).to have_css "label.completed", text: name
+      expect(task.reload).to be_completed
+    end
+  end
+  
+  def expect_incomplete_task(name)
+    aggregate_failures do
+      expect(page).to_not have_css "label.completed", text: name
+      expect(task.reload).not_to be_completed
+    end
   end
 end
