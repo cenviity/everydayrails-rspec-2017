db5ece928534c597fc6430baa50aa4c5f2e133f3
Vincent Yim <cenviity@gmail.com>
2017-08-30 19:35:51 +0000

    Add failure messages

--- spec/support/matchers/content_type.rb
+++ spec/support/matchers/content_type.rb
@@ -7,6 +7,24 @@ RSpec::Matchers.define :have_content_type do |expected|
     end
   end
   
+  failure_message do |actual|
+    sprintf('Expected "%s (%s)" to be Content Type "%s" (%s)',
+      content_type(actual.content_type),
+      actual.content_type,
+      content_type(expected),
+      expected
+    )
+  end
+  
+  failure_message_when_negated do |actual|
+    sprintf('Expected "%s (%s)" to not be Content Type "%s" (%s)',
+      content_type(actual.content_type),
+      actual.content_type,
+      content_type(expected),
+      expected
+    )
+  end
+  
   def content_type(type)
     types = {
       html: "text/html",
