6356104c63ea83509f9ca42d17b6a32fc4542979
Vincent Yim <cenviity@gmail.com>
2017-08-30 16:34:13 +0000

    Add specs for creating a project

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :request do
-  describe "GET /projects" do
-    it "works! (now write some real specs)" do
-      get projects_index_path
-      expect(response).to have_http_status(200)
+  context "as an authenticated user" do
+    before do
+      @user = create(:user)
+    end
+
+    context "with valid attributes" do
+      it "adds a project" do
+        project_params = attributes_for(:project)
+        sign_in @user
+        expect {
+          post projects_path, params: { project: project_params }
+        }.to change(@user.projects, :count).by(1)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not add a project" do
+        project_params = attributes_for(:project, :invalid)
+        sign_in @user
+        expect {
+          post projects_path, params: { project: project_params }
+        }.to_not change(@user.projects, :count)
+      end
     end
   end
 end
